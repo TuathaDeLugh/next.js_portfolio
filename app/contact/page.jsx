5df00eb388dfed4504be4161254e51f5e2f971c0
Umang Sailor <ursailor@gmail.com>
2023-09-25 13:06:09 +0530

    contact form backend working

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,3 +1,4 @@
+import ContactPage from '@/components/ContactPage';
 import Image from 'next/image'
 import React from 'react'
 import { AiFillGithub,AiFillFacebook,AiOutlineWhatsApp,AiOutlineTwitter,AiFillLinkedin } from "react-icons/ai";
@@ -14,87 +15,9 @@ function Contact() {
 
 
         <div className="w-full lg:w-8/12 px-4">
-        <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-green-50 border-0">
-        <div className="rounded-t bg-white mb-0 px-14 py-5">
-          <div className="text-center flex justify-between">
-            <h6 className="text-gray-600 uppercase text-3xl font-semibold">Contact Me</h6> 
-          </div>
-        </div>
-        <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
-          <form>
-            
-            <div className="flex flex-wrap mt-5">
-              <div className="w-full lg:w-6/12 px-4">
-                <div className="relative w-full mb-3">
-                  <label
-                    className="block uppercase text-gray-600 text-xs font-bold mb-2"
-                    htmlFor="grid-password"
-                    >
-                    Full Name
-                  </label>
-                  <input
-                    type="text"
-                    className="border-0 px-3 py-2 placeholder-gray-300 text-black bg-white rounded text-base shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                    placeholder='Umang Sailor' required
-                    /> 
-                </div>
-              </div>
-              <div className="w-full lg:w-6/12 px-4">
-                <div className="relative w-full mb-3">
-                  <label
-                    className="block uppercase text-xs font-bold mb-2"
-                    htmlFor="grid-password"
-                    >
-                    Email Address
-                  </label>
-                  <input
-                    type="text"
-                    className="border-0 px-3 py-2 placeholder-gray-300 text-black bg-white rounded text-base shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                    placeholder='umangsailor@hotmail.com' required
-                    />
-                </div>
-              </div>
-              <div className="w-full lg:w-12/12 px-4">
-                <div className="relative w-full mb-3">
-                  <label
-                    className="block uppercase text-gray-600 text-xs font-bold mb-2"
-                    >
-                    Subject
-                  </label>
-                  <input
-                    type="text"
-                    className="border-0 px-3 py-2 placeholder-gray-300 text-black bg-white rounded text-base shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                    placeholder='Colabrate with you' required
-                    />
-              </div>
-              </div>
-              <div className="w-full lg:w-12/12 px-4">
-                <div className="relative w-full mb-3">
-                  <label
-                    className="block uppercase text-gray-600 text-xs font-bold mb-2"
-                    >
-                    Detail
-                  </label>
-                  <textarea
-                    type="text"
-                    className="border-0 px-3 py-2 placeholder-gray-300 text-gray-600 bg-white rounded text-base shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                    rows="8"
-                    required
-                    placeholder='Hey I want To colarabrate with you on any Next.js project '
-                    ></textarea>
-                </div>
-              </div>
-              <div className="w-full lg:w-12/12 px-4">
-                <div className="relative w-full mb-3">
-                  <button className=' bg-green-600  text-white border rounded px-6 py-2 hover:bg-green-900'> Submit</button>
-                </div>
-              </div>
-            </div>
-          </form>
-        </div>
-      </div>
+          <ContactPage/>
         <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg p-8 bg-green-50 border-0">        
-        <iframe className='rounded' src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d14876.689800341675!2d72.77946530537108!3d21.225009164083545!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjHCsDEzJzEzLjQiTiA3MsKwNDYnNDkuMyJF!5e0!3m2!1sgu!2sin!4v1695572227814!5m2!1sgu!2sin"  height="550" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <iframe className='rounded' src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d14876.689800341675!2d72.77946530537108!3d21.225009164083545!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjHCsDEzJzEzLjQiTiA3MsKwNDYnNDkuMyJF!5e0!3m2!1sgu!2sin!4v1695572227814!5m2!1sgu!2sin"  height="550" loading="lazy" ></iframe>
         </div>
         </div>
         <div className="w-full lg:w-4/12 px-4">
---
+++ components/ContactPage.jsx
@@ -0,0 +1,127 @@
+"use client";
+import React from 'react'
+
+import { useState } from 'react'
+import { useRouter } from "next/navigation";
+import {toast } from 'react-toastify';
+
+function ContactPage() {
+    const [fullname,setFullname] = useState("");
+    const [email,setEmail] = useState("");
+    const [subject,setSubject] = useState("");
+    const [details,setDetails] = useState("");
+  
+    const router = useRouter();
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+  
+    //   if (!fullname||!email||!subject||!details) {
+    //     toast.error('Data dal de');
+    //     return;
+    //   }
+      try {
+        const res = await fetch(`/api/email`, {
+          method: "POST",
+          headers: {
+            "Content-type": "application/json",
+          },
+          body: JSON.stringify({fullname,email,subject,details }),
+        });
+        // console.log({ fullname,email,subject,details });
+        if (res.ok) {
+            // router.refresh();
+            router.push("/");
+            toast.success('Message Sent Successfully');
+          return;
+        } else {
+          throw new Error("Failed to add Email");
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+  return (
+    <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-green-50 border-0">
+        <div className="rounded-t bg-white mb-0 px-14 py-5">
+          <div className="text-center flex justify-between">
+            <h6 className="text-gray-600 uppercase text-3xl font-semibold">Contact Me</h6> 
+          </div>
+        </div>
+        <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
+          <form onSubmit={handleSubmit}>
+            
+            <div className="flex flex-wrap mt-5">
+              <div className="w-full lg:w-6/12 px-4">
+                <div className="relative w-full mb-3">
+                  <label
+                    className="block uppercase text-gray-600 text-xs font-bold mb-2"
+                    htmlFor="grid-password"
+                    >
+                    Full Name
+                  </label>
+                  <input
+                    type="text" onChange={(e)=> setFullname(e.target.value) }value={fullname} 
+                    className="border-0 px-3 py-2 placeholder-gray-400 text-black bg-white rounded text-base shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                    placeholder='Umang Sailor' required
+                    /> 
+                </div>
+              </div>
+              <div className="w-full lg:w-6/12 px-4">
+                <div className="relative w-full mb-3">
+                  <label
+                    className="block uppercase text-xs font-bold mb-2"
+                    htmlFor="grid-password"
+                    >
+                    Email Address
+                  </label>
+                  <input
+                    type="text" onChange={(e)=> setEmail(e.target.value) }value={email} 
+                    className="border-0 px-3 py-2 placeholder-gray-400 text-black bg-white rounded text-base shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                    placeholder='umangsailor@hotmail.com' required
+                    />
+                </div>
+              </div>
+              <div className="w-full lg:w-12/12 px-4">
+                <div className="relative w-full mb-3">
+                  <label
+                    className="block uppercase text-gray-600 text-xs font-bold mb-2"
+                    >
+                    Subject
+                  </label>
+                  <input
+                    type="text" onChange={(e)=> setSubject(e.target.value) }value={subject} 
+                    className="border-0 px-3 py-2 placeholder-gray-400 text-black bg-white rounded text-base shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                    placeholder='Colabrate with you' required
+                    />
+              </div>
+              </div>
+              <div className="w-full lg:w-12/12 px-4">
+                <div className="relative w-full mb-3">
+                  <label
+                    className="block uppercase text-gray-600 text-xs font-bold mb-2"
+                    >
+                    Detail
+                  </label>
+                  <textarea
+                    type="text" onChange={(e)=> setDetails(e.target.value) }value={details} 
+                    className="border-0 px-3 py-2 placeholder-gray-400 text-black bg-white rounded text-base shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                    rows="8"
+                    required
+                    placeholder='Hey I want To colarabrate with you on any Next.js project '
+                    ></textarea>
+                </div>
+              </div>
+              <div className="w-full lg:w-12/12 px-4">
+                <div className="relative w-full mb-3">
+                  <button type='submit' className=' bg-green-600  text-white border rounded px-6 py-2 hover:bg-green-900'> Submit</button>
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+  )
+}
+
+export default ContactPage
