a93dfc04a7401573a42d9ca2ef1ffa672e67c079
Umang Sailor <ursailor@gmail.com>
2023-09-27 14:28:57 +0530

    mobile devide nav toggle fix

--- components/Adminlogout.jsx
+++ components/Adminlogout.jsx
@@ -6,12 +6,14 @@ import Link from "next/link"
     const { data: session, status } = useSession()
     if (status === "authenticated") {
       return (<>
-      <li><Link href={"/admin"} 
-      className="text-l text-slate-900 py-5 px-5 text-center  border-b-2 md:border-b-0  border-slate-700  md:hover:text-slate-500 md:hover:font-semibold md:hover:bg-transparent">
+      <li className="text-l text-slate-900 py-5 px-5 text-center  border-b-2 md:border-b-0  border-slate-700  md:hover:text-slate-500 md:hover:font-semibold md:hover:bg-transparent">
+        <Link href={"/admin"} onClick={() => setNavbar(!navbar)}
+      >
           Admin
         </Link>
-        </li>
-      <li><button className="text-l text-white rounded bg-green-500 hover:border-2 hover:border-green-500  md:my-3 md:py-2 md:px-2 py-5 px-6 text-center  border-b-2 md:border-b-0   md:hover:text-green-500  md:hover:bg-transparent"
+        </li >
+      <li className="text-l text-white rounded bg-green-500 hover:border-2 hover:border-green-500  md:my-3 md:py-2 md:px-2 py-5 px-6 text-center  border-b-2 md:border-b-0   md:hover:text-green-500  md:hover:bg-transparent">
+        <button
           onClick={() => signOut()}>
           Log Out
         </button>
