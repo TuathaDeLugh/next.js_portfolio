6f5f9408155011ae3279be17ab0ab16ce7928540
Umang Sailor <ursailor@gmail.com>
2023-09-29 11:07:15 +0530

    lol

--- components/Seschack.jsx
+++ components/Seschack.jsx
@@ -1,6 +1,7 @@
 "use client"
 import { useSession } from 'next-auth/react'
 import LoginForm from './Loginform'
+import {redirect} from "next/navigation"
 
 function Seschack({children}) {
     const { data: session} = useSession()
@@ -16,7 +17,7 @@ function Seschack({children}) {
             { 
              ! session &&(
               <>
-            <LoginForm/>
+            {redirect("/login")}
               </>)
                 }   
     </>
