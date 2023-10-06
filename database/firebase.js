12312b544f7f3c6e6918172e33ee005e694c3868
Umang Sailor <ursailor@gmail.com>
2023-10-06 20:43:42 +0530

    added firebase connection

---
+++ database/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+const firebaseConfig = {
+  apiKey:  process.env.apiKey,
+  authDomain:  process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId:  process.env.messagingSenderId,
+  appId:  process.env.appid,
+  measurementId: process.env.measurementId
+};
+
+const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app);
