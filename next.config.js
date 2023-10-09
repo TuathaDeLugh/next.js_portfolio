a2885d5a016aedee2d35725167325e2a94c79fc0
Umang Sailor <ursailor@gmail.com>
2023-10-09 13:05:24 +0530

    PWA fix (now not work in dev mode)

--- next.config.js
+++ next.config.js
@@ -1,18 +1,8 @@
 // /** @type {import('next').NextConfig} */
-// const withPWA = require('next-pwa')({
-//     dest: 'public',
-//     register: true,
-//     skipWaiting: true,
-//     disable: process.env.NODE_ENV === 'development'
-//   })
-
-// const nextConfig = {
-//     reactStrictMode: true
-// }
-
-// module.exports = withPWA(nextConfig)
-const withPWA = require('next-pwa')({
-  dest: 'public'
+const withPWA = require('next-pwa')(
+  {
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development'
 })
 
 module.exports = withPWA({
