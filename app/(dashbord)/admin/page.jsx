ddf65049348a7c6441571c9b886d06c84403bb4f
Umang Sailor <ursailor@gmail.com>
2023-10-22 15:51:46 +0530

    fix deleted things

--- app/(dashbord)/admin/page.jsx
+++ app/(dashbord)/admin/page.jsx
@@ -3,20 +3,10 @@ import React from 'react'
 import { authOptions } from '@/app/(all)/api/auth/[...nextauth]/route'
 import { getServerSession } from 'next-auth'
 import { redirect } from 'next/navigation'
-import LoginForm from '@/components/InsertForm/LoginForm'
+
 
 export default async function Admin() {
 	const session = await getServerSession(authOptions)
-
-	// redirect if already logged in as admin
 	if (session)
 		return redirect('/admin/dashbord')
-
-//   return (
-//     <>
-
-// <LoginForm/>
-
-//     </>
-//   )
 }
